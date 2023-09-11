8ccb6c6c4f48938925a1f81156867d174cf6beec
majdabbas9 <majd.abbas999@gmail.com>
2023-09-11 20:08:46 +0300

    PieChart

---
+++ frontend/src/charts/PieChart.js
@@ -0,0 +1,24 @@
+import CanvasJSReact from '@canvasjs/react-charts';
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+function PieChart({rulesCounters}){
+    
+    const options = {
+        animationEnabled: true,
+        exportEnabled: true,
+        theme: "dark1", // "light1", "dark1", "dark2"
+        data: [{
+            type: "pie",
+            indexLabel: "{name}: {rate}%",		
+            startAngle: -90,
+            dataPoints: rulesCounters
+        }]}
+        return (
+            <>
+            <div>
+                {<CanvasJSChart options = {options} />}
+            </div>
+            </>
+            );
+}
+export default PieChart;
