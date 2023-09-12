16ab158d7bcc8eba2268fef3a5ce17b0205ffff7
majdabbas9 <majd.abbas999@gmail.com>
2023-09-12 22:03:28 +0300

    new 9

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import PieChart from './charts/PieChart'
+import PerXScatter from './charts/PerXScatter';
 
+
+const arr= [
+  {
+      "x": "2023-09-02T18:00:00.000Z",
+      "y": 1
+  },
+  {
+      "x": "2023-09-02T18:15:00.000Z",
+      "y": 1
+  },
+  {
+      "x": "2023-09-02T18:30:00.000Z",
+      "y": 0
+  },
+  {
+      "x": "2023-09-02T18:45:00.000Z",
+      "y": 1
+  },
+  {
+      "x": "2023-09-02T19:00:00.000Z",
+      "y": 0
+  }
+]
 function App() {
+  const divStyle = {
+    width: '50%'
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div style={divStyle}>
+    <PerXScatter arr={arr}/>
+    <PerXScatter arr={arr}/>
+    <PerXScatter arr={arr}/>
     </div>
+    </>
   );
 }
 
